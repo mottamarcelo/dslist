41e7952f09e8296a51a97925c0636b05cbe095f1
Marcelo Motta <marcelorodriguesmotta@gmail.com>
2025-08-21 14:52:08 -0300

    Remove emphasized items

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -2,7 +2,6 @@ package com.devsuperior.dslist.services;
 
 import java.util.List;
 
-import org.hibernate.id.IntegralDataTypeHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
